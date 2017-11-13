94107c1151123fcda9ae2decdc374f8d2db3d7aa
Mẫn <vanman.lqd@gmail.com>
2017-11-13 14:22:44 +0700

    Add code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num);
+            if (num == 1) Console.WriteLine("Sunday");
+            else if (num == 2) Console.WriteLine("Monday");
+            else if (num == 3) Console.WriteLine("Tuesday");
+            else if (num == 4) Console.WriteLine("Wednesday");
+            else if (num == 5) Console.WriteLine("Thursday");
+            else if (num == 6) Console.WriteLine("Friday");
+            else if (num == 7) Console.WriteLine("Saturday");
+            else
+            {
+                Console.WriteLine("Invalid day");
+            }
+            Console.ReadKey();
+        }
+    }
+}
