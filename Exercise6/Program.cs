1ba7d06d7979feb7508e4f3d12b2089262a55405
Mẫn <vanman.lqd@gmail.com>
2017-11-13 17:24:11 +0700

    exercise6

--- Exercise6/Program.cs
+++ Exercise6/Program.cs
@@ -10,8 +10,50 @@ namespace Exercise6
     {
         static void Main(string[] args)
         {
-
+            Console.Write("Enter the first edge: ");
+            var firstEdge = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second edge: ");
+            var secondEdge = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the third edge: ");
+            var thirdEdge = Convert.ToInt32(Console.ReadLine());
+            if (IsEdgesOfTriangle(firstEdge, secondEdge, thirdEdge))
+            {
+                Console.WriteLine(GetTypeOfTriangle(firstEdge, secondEdge, thirdEdge));
+            }
+            else
+            {
+                Console.WriteLine("Is not a triangle!");
+            }
             Console.ReadKey();
+        }
+        static bool IsEdgesOfTriangle(int firstEdge, int secondEdge, int thirdEdge)
+        {
+            return (firstEdge + secondEdge > thirdEdge && secondEdge + thirdEdge > firstEdge &&
+                    thirdEdge + firstEdge > secondEdge);
+        }
+        static string GetTypeOfTriangle(int firstEdge, int secondEdge, int thirdEdge)
+        {
+            if (firstEdge == secondEdge && secondEdge == thirdEdge)
+            {
+                return "Equilateral Triangle";
+            }
+            if (firstEdge == secondEdge || secondEdge == thirdEdge || thirdEdge == firstEdge)
+            {
+                return "Isosceles Triangle";
+            }
+            var maxEdge = (firstEdge > secondEdge && firstEdge > thirdEdge)
+                ? firstEdge
+                : (secondEdge > thirdEdge)
+                    ? secondEdge
+                    : thirdEdge;
+            var minEdge = (firstEdge < secondEdge && firstEdge < thirdEdge)
+                ? firstEdge
+                : (secondEdge < thirdEdge)
+                ? secondEdge
+                : thirdEdge;
+            var edge = firstEdge + secondEdge + thirdEdge - maxEdge - minEdge;
+            if (maxEdge * maxEdge == edge * edge + minEdge * minEdge) return "Square Triangle";
+            return "Normal Triangle";
         }
     }
 }
