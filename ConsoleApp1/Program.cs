60629e9e199bee15cc9372849ac888c0ea99aaaf
Mẫn <vanman.lqd@gmail.com>
2017-11-13 14:49:14 +0700

    switch case

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,6 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(num);
             if (num == 1) Console.WriteLine("Sunday");
             else if (num == 2) Console.WriteLine("Monday");
             else if (num == 3) Console.WriteLine("Tuesday");
@@ -22,6 +21,35 @@ namespace ConsoleApp1
             else
             {
                 Console.WriteLine("Invalid day");
+            }
+
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day");
+                    break;
+
             }
             Console.ReadKey();
         }
