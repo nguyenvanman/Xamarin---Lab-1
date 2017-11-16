ae82f46c087f1156106ba3ff645df11470948c91
Mẫn <vanman.lqd@gmail.com>
2017-11-16 20:51:57 +0700

    Complete OOP Exercise

---
+++ OOPExercise6/Fraction.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExercise6
+{
+    class Fraction
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public Fraction(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public int GetGreatestCommonDivisor(int number1, int number2)
+        {
+            return (number2 == 0) ? number1 : GetGreatestCommonDivisor(number2, number1 % number2);
+        }
+
+        public Fraction Minimal()
+        {
+            var greatestCommonDivisor = GetGreatestCommonDivisor(A, B);
+            A = A / greatestCommonDivisor;
+            B = B / greatestCommonDivisor;
+            return this;
+        }
+
+        public override string ToString() => $"{A}:{B}";
+        public static Fraction operator + (Fraction fraction1, Fraction fraction2)
+        {
+            return (new Fraction(fraction1.A * fraction2.B + fraction2.A * fraction1.B,
+                fraction1.B * fraction2.B)).Minimal();
+        }
+
+        public static Fraction operator - (Fraction fraction1, Fraction fraction2)
+        {
+            return (new Fraction(fraction1.A * fraction2.B - fraction2.A * fraction1.B,
+                fraction1.B * fraction2.B)).Minimal();
+        }
+        public static Fraction operator * (Fraction fraction1, Fraction fraction2)
+        {
+            return (new Fraction(fraction1.A * fraction2.A, fraction1.B * fraction2.B)).Minimal();
+        }
+        public static Fraction operator / (Fraction fraction1, Fraction fraction2)
+        {
+            return (new Fraction(fraction1.A * fraction2.B, fraction1.B * fraction2.A));
+        }
+        public static bool operator > (Fraction fraction1, Fraction fraction2)
+        {
+            return (fraction1 - fraction2).A * (fraction1 - fraction2).B > 0;
+        }
+        public static bool operator < (Fraction fraction1, Fraction fraction2)
+        {
+            return fraction2 > fraction1;
+        }
+        public static bool operator >= (Fraction fraction1, Fraction fraction2)
+        {
+            return !(fraction1 < fraction2);
+        }
+        public static bool operator <= (Fraction fraction1, Fraction fraction2)
+        {
+            return !(fraction1 > fraction2);
+        }
+        public static Fraction operator ! (Fraction fraction)
+        {
+            return new Fraction(fraction.B, fraction.A).Minimal();
+        }
+    }
+}
---
+++ OOPExercise6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fraction1 = (new Fraction(5, 9)).Minimal();
+            Fraction fraction2 = (new Fraction(4, 7).Minimal());
+            Console.WriteLine("Fraction 1 : " + fraction1.ToString());
+            Console.WriteLine("Fraction 2 : " + fraction2.ToString());
+
+            Fraction addFraction = fraction1 + fraction2;
+            Fraction subFraction = fraction1 - fraction2;
+            Fraction mulFraction = fraction1 * fraction2;
+            Fraction divFraction = fraction1 / fraction2;
+            Fraction invertFraction = !fraction1;
+
+            Console.WriteLine("operator + : " + addFraction.ToString());
+            Console.WriteLine("operator - : " + subFraction.ToString());
+            Console.WriteLine("operator * : " + mulFraction.ToString());
+            Console.WriteLine("operator / : " + divFraction.ToString());
+            Console.WriteLine("operator ! : " + invertFraction.ToString());
+
+            Console.WriteLine("operator > : " + (fraction1 > fraction2));
+            Console.WriteLine("operator < : " + (fraction1 < fraction2));
+            Console.WriteLine("operator >= : " + (fraction1 >= fraction2));
+            Console.WriteLine("operator <= : " + (fraction1 <= fraction2));
+
+            Console.ReadKey();
+        }
+    }
+}
